cfa2e6048bb1bb7c22402a24e2b106834a8e1e90
HannaKush <madam.kushnir1999@gmail.com>
2023-10-24 12:37:07 -0400

    Add all my files

---
+++ routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
+use App\Http\Controllers\PostController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::view('/', 'welcome');
+
+Route::redirect('/home', '/', 302);
+
+Route::get('/test', TestController::class );
+
+
+Route::get('posts', [PostController::class, 'index'])->name('posts');
+
+Route::get('posts/create', [PostController::class, 'create'])->name('posts.create');
+
+Route::post('posts', [PostController::class,'store'])->name('posts.store');
+
+Route::get('posts/{post}', [PostController::class,'show'])->name('posts.show');
+
+Route::get('posts/{post}/edit', [PostController::class,'edit'])->name('posts.edit');
+
+Route::put('posts/{post}', [PostController::class,'update'])->name('posts.update');
+
+Route::delete('posts/{post}', [PostController::class,'delete'])->name('posts.delete');
+
+
+
+
+Route::fallback(function (){
+    return 'Fallback';
+});
